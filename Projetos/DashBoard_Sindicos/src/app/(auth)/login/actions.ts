52a5b3cb1692d952a9e5cb257158290fef9f2ce8
mandrade25 <104328447+mandrade25@users.noreply.github.com>
2026-04-16 13:30:34 -0300

    Create actions.ts

---
+++ Projetos/DashBoard_Sindicos/src/app/(auth)/login/actions.ts
@@ -0,0 +1,29 @@
+"use server"
+
+import { signIn } from "@/auth"
+import { AuthError } from "next-auth"
+import { redirect } from "next/navigation"
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn("credentials", {
+      email: formData.get("email"),
+      password: formData.get("password"),
+      redirect: false,
+    })
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return "Credenciais inválidas."
+        default:
+          return "Algo deu errado."
+      }
+    }
+    throw error
+  }
+  redirect("/dashboard")
+}
